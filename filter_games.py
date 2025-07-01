0cb993ebb221604c5b26715a56db8ec0bdc13cef
LichessBotLi <102945249+LichessBotLi@users.noreply.github.com>
2025-07-01 18:59:33 +0530

    Create filter_games.py

---
+++ filter_games.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import chess.pgn, sys, pathlib
+
+IN  = pathlib.Path("white.pgn")
+OUT = pathlib.Path("whiter.pgn")
+BOT = "Nikitosikbot_v2"
+
+with IN.open() as fin, OUT.open("w") as fout:
+    while (g := chess.pgn.read_game(fin)) is not None:
+        if g.headers.get("White") == BOT and g.headers.get("Result") in {"1-0", "1/2-1/2"}:
+            print(g, file=fout, end="\n\n")
